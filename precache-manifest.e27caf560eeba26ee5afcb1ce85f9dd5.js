b7da51c1298e6a2fab80dfe04e15d2f96bdf0a2a
Rudy Jauregui <rjaureguijr@gmail.com>
2020-09-08 12:06:21 -0500

    Updates

---
+++ precache-manifest.e27caf560eeba26ee5afcb1ce85f9dd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d292acf0a2a19ac203d141f03a9c085",
+    "url": "/food-find/index.html"
+  },
+  {
+    "revision": "236c6c16d86718e928ce",
+    "url": "/food-find/static/css/main.436f594e.chunk.css"
+  },
+  {
+    "revision": "9925d90d1471d1ce66f7",
+    "url": "/food-find/static/js/2.99d41395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-find/static/js/2.99d41395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236c6c16d86718e928ce",
+    "url": "/food-find/static/js/main.5ac2e21d.chunk.js"
+  },
+  {
+    "revision": "254ccf905c44ec5d7853",
+    "url": "/food-find/static/js/runtime-main.38b3be83.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/food-find/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/food-find/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
